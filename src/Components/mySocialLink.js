43c95ea1599ce50d4be49683600b61f23f56b3c3
John Paul Dela Rosa <delarosapauldelarosa@gmail.com>
2025-02-21 18:44:46 +0800

    ito na

--- src/Components/mySocialLink.js
+++ src/Components/mySocialLink.js
@@ -4,22 +4,22 @@ import React from "react";
 
 function Container () {
     return (
-        <div className="w-xs h-fit bg-grey800 text-center">
-            <img className="size-24 mx-auto rounded-full" src="/assets/images/avatar-jessica.jpeg"></img>
-            <h1>Jessica Randall</h1>
-            <h3 className="text-customGreen">London, United Kingdom</h3>
+        <div className="w-xs h-fit bg-grey800 text-center p-8 rounded-md">
+            <img className="size-20 mx-auto rounded-full" src="/assets/images/avatar-jessica.jpeg"></img>
+            <h1 className="text-2xl my-2 ">Jessica Randall</h1>
+            <h3 className="text-customGreen text-sm font-medium ">London, United Kingdom</h3>
 
-            <p>"Front-end developer and avid reader."</p>
+            <p className="text-sm my-4 ">"Front-end developer and avid reader."</p>
             
-            <ul>
-                <li>GitHub</li>
-                <li>Frontend Mentor</li>
-                <li>LinkedIn</li>
-                <li>Twitter</li>
-                <li>Instagram</li>
+            <ul className="text-sm font-medium">
+                <li className="py-3 my-4 rounded-md bg-neutral-700 hover:text-black hover:bg-lime-400">Frontend Mentor</li>
+                <li className="py-3 my-4 rounded-md bg-neutral-700 hover:text-black hover:bg-lime-400">LinkedIn</li>
+                <li className="py-3 my-4 rounded-md bg-neutral-700 hover:text-black hover:bg-lime-400">GitHub</li>
+                <li className="py-3 my-4 rounded-md bg-neutral-700 hover:text-black hover:bg-lime-400">Twitter</li>
+                <li className="py-3 my-4 rounded-md bg-neutral-700 hover:text-black hover:bg-lime-400">Instagram</li>
             </ul>
             
-            <div class="attribution">
+            <div className="attribution text-xs">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
                 Coded by <a href="#">John Paul Dela Rosa</a>.
             </div>
